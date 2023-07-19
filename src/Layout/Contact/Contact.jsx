1787ccf90a5567108be9685549b697bf0f9fd987
Bernard Konan <devwithbernard@gmail.com>
2023-07-19 17:24:51 +0000

    fix iframe title property

--- src/Layout/Contact/Contact.jsx
+++ src/Layout/Contact/Contact.jsx
@@ -106,7 +106,7 @@ export const Contact =()=>{
                     </div>
                     <div className="contact map">
                         <iframe
-                            title=""
+                            title="iframe-bernard-Konan"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.418314005617!2d-4.067742824289624!3d5.352956735656527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1ebf3be52d4ef%3A0xf3364bbfb4a463ac!2sColl%C3%A8ge%20La%20Sorbonne!5e0!3m2!1sfr!2sci!4v1689784594489!5m2!1sfr!2sci"
                            allowFullScreen="" loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"></iframe>
