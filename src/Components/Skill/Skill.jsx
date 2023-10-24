71d821e9a380d1091636c2b4ed982a6a1459cd35
Bernard Konan <devwithbernard@gmail.com>
2023-10-24 07:46:31 +0000

    fix skill links

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,15 +1,17 @@
-import "./skill.scss"
-export const Skill = ({skill}) =>{
-    return (
-        <a href="/skills" className="link">
-            <img className="img" src={skill.img} alt={skill.title}/>
-            <section className="section">
-                <h5>{skill.title}</h5>
-                <p>{skill.description}</p>
-                <div className="tags">
-                    {skill.tags.map((tag,index) => (<span key={index}>{'#'+tag}</span>))}
-                </div>
-            </section>
-        </a>
-    )
-}
+import "./skill.scss";
+export const Skill = ({ skill }) => {
+  return (
+    <div className="link">
+      <img className="img" src={skill.img} alt={skill.title} />
+      <section className="section">
+        <h5>{skill.title}</h5>
+        <p>{skill.description}</p>
+        <div className="tags">
+          {skill.tags.map((tag, index) => (
+            <span key={index}>{"#" + tag}</span>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
